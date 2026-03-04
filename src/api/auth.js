5e14480aebcc3318e1501b9a72901a6e1251867b
Der Yu Tu <ann199560@gmail.com>
2026-03-04 16:22:44 +0800

    更新 mock-api/package.json

--- src/api/auth.js
+++ src/api/auth.js
@@ -1,9 +1,6 @@
 import api from "./api";
 
 export async function registerUser(payload) {
-  console.log("registerUser payload:", payload);
-  console.log("request url should be:", `${api.defaults.baseURL}/users`);
-
   const res = await api.post("/users", payload);
   return res.data;
 }
